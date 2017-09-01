9f60307cfbc8ef5d3ab876b04610412ec7832baf
Matt License <matt.license@neueda.com>
2017-09-01 14:11:15 +0100

    Making the domain / subdomain to alter configurable from application.properties.

--- src/main/java/com/licensem/dnsimple/Application.kt
+++ src/main/java/com/licensem/dnsimple/Application.kt
@@ -3,6 +3,7 @@ package com.licensem.dnsimple
 import com.licensem.dnsimple.model.RecordType
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -21,12 +22,15 @@ fun main(args: Array<String>) {
     AnnotationConfigApplicationContext(Application::class.java)
 }
 
-@Component
 @Configuration
 @ComponentScan(basePackages = arrayOf("com.licensem.dnsimple") )
 @PropertySource("classpath:application.properties")
 @EnableScheduling
-open class Application(@Autowired private var api: DNSimpleApi) {
+open class Application(
+    @Autowired private var api: DNSimpleApi,
+    @Value("\${dnsimple.api.domain}") private val domainName: String,
+    @Value("\${dnsimple.api.subDomain}") private val subDomain: String
+) {
     private val LOG = LogManager.getLogger(this.javaClass)!!
 
     @Bean
@@ -35,13 +39,13 @@ open class Application(@Autowired private var api: DNSimpleApi) {
     }
 
     /**
-     * Runs every hour to check whether the external IP address has changed. If it has, updates the A record
-     * for dl.mattlicense.co.uk
+     * Runs every hour to check whether the external IP address has changed. If it has, updates the A record for the
+     * specified domain / subDomain
      */
     @Scheduled(fixedDelay = 3600000L)
     fun checkIpAddress() {
         val domains = api.getDomains()
-        val domain = domains.firstOrNull { it.name == "mattlicense.co.uk" }
+        val domain = domains.firstOrNull { it.name == domainName }
         if(domain != null) {
             LOG.info("Found details for ${domain.name}")
             val records = api.getRecords(domain.name!!)
@@ -50,20 +54,20 @@ open class Application(@Autowired private var api: DNSimpleApi) {
             val ip = BufferedReader(InputStreamReader(URL("http://checkip.amazonaws.com").openStream())).readLine()
             LOG.info("Current external IP address is $ip")
 
-            val downloadsRecord = records.firstOrNull { it.name == "dl" }
+            val downloadsRecord = records.firstOrNull { it.name == subDomain }
             if (downloadsRecord == null) {
-                LOG.info("dl.mattlicense.co.uk doesn't exist, creating it")
-                api.createRecord(domain.name!!, RecordType.A, "dl", ip)
+                LOG.info("$subDomain.$domainName doesn't exist, creating it")
+                api.createRecord(domain.name!!, RecordType.A, subDomain, ip)
             } else {
                 if (downloadsRecord.content != ip) {
-                    LOG.info("IP changed from ${downloadsRecord.content} to $ip")
+                    LOG.info("IP changed from ${downloadsRecord.content} to $ip on $subDomain.$domainName")
                     api.updateRecord(domain.name!!, downloadsRecord.id!!, ip)
                 } else {
                     LOG.info("No IP change detected")
                 }
             }
         } else {
-            LOG.warn("Couldn't find domain mattlicense.co.uk")
+            LOG.warn("Couldn't find domain $domainName")
         }
     }
 }
